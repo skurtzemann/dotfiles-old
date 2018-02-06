f960d092780bb1fc7dd4b89d1411bca861837ac0
Sébastien Kurtzemann <sebastien.k@capkopper.fr>
2018-02-06 01:20:01 +0100

    [oh-my-zsh] Add go config

---
+++ .oh-my-zsh/custom/go.zsh
@@ -0,0 +1,5 @@
+
+export GOPATH="/Users/sebastien/Documents/Work/Sources/Go"
+
+export PATH=$PATH:/usr/local/go/bin
+export PATH=$PATH:$GOPATH/bin
--- setup.sh
+++ setup.sh
@@ -11,6 +11,7 @@ for f in \
     .zshrc \
     .oh-my-zsh/custom/ansible.zsh \
     .oh-my-zsh/custom/docker.zsh \
+    .oh-my-zsh/custom/go.zsh \
     .oh-my-zsh/custom/k8s.zsh; do
     if [ -f "$HOME/$f" ]; then rm "$HOME/$f"; fi
     ln -sf "$SCRIPT_DIR/$f" "$HOME/$f"
